ae30a2d8a2e26d49d476a44b1ccf23ddfadd9f9a
tang0228 <2903474904@qq.com>
2025-04-10 21:33:32 +0800

    test reactive

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
 import showDialog from "./components/showDialog"
+import { reactive } from "vue"
 
 const toggle = () => {
   const close = showDialog('asdsada', () => {
     close()
   })
 }
+
+let obj = reactive({
+  a: {
+    b: 1
+  }
+})
+
+const changeData = () => {
+  obj.a.b = 2
+}
 </script>
 
 <template>
   <button @click="toggle">显示</button>
+  <div>{{ obj.a.b }}</div>
+  <button @click="changeData">改变数据</button>
 </template>
 
 <style scoped></style>
